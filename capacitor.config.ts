3c6b1a8fa3360beec42c45084906f782e62e6fe0
Nathan <nathan@naykel.com.au>
2023-05-14 15:43:30 +1000

    add assets

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: '2701_happy_hour',
+  appName: 'Happy Hour',
   webDir: 'www',
   server: {
     androidScheme: 'https'
