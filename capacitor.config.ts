57da73c675afd708fba597663d5d9c2abcc3f370
Nathan <nathan@naykel.com.au>
2023-05-14 15:40:46 +1000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '2701_happy_hour',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
